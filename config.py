9225fb0b78205f1007ad846e5e5c7ebf6acb82a9
Yuri D <breakeneck@gmail.com>
2020-07-11 18:17:10 +0000

    Implement slider move, refactored qt imports

--- config.py
+++ config.py
@@ -38,7 +38,7 @@ class Config:
         self.config['window'] = self.w.geometry().getRect()
         self.config['splitter'] = self.w.splitter.sizes()
         self.config['theme'] = self.w.themeCombo.currentText()
-        self.config['column_sizes'] = [self.w.tableView.columnWidth(i) for i in range(0, self.w.gridModel.columnCount())]
+        self.config['column_sizes'] = [self.w.tableView.columnWidth(i) for i in range(0, self.w.tableModel.columnCount())]
         json.dump(self.config, open('config.json', 'w'))
 
     def setLibraryDir(self, directory):
--- main.py
+++ main.py
@@ -1,17 +1,14 @@
-import sys  # sys нужен для передачи argv в QApplication
+import sys
 import os
-from PyQt5.QtCore import Qt
-from PyQt5 import QtWidgets
-from PyQt5.QtCore import QModelIndex
-from PyQt5.QtCore import QDir
-from PyQt5.QtGui import QPalette, QColor
-from PyQt5.QtSql import QSqlDatabase, QSqlTableModel
-from PyQt5.QtGui import QStandardItemModel
-# from PyQt5.QtGui import QIcon
-# from PyQt5.QtCore import
-from PyQt5.QtWidgets import QAbstractItemView
 
-import design  # Это наш конвертированный файл дизайна
+from PyQt5 import QtWidgets, QtGui, QtCore, QtSql
+
+# from PyQt5.QtCore import Qt, QModelIndex, QDir
+# from PyQt5.QtGui import QPalette, QColor, QStandardItemModel, QIcon
+# from PyQt5.QtSql import QSqlDatabase, QSqlTableModel
+# from PySide2 import QtWidgets, QtGui, QtCore
+
+import design
 import config
 import library
 import player
@@ -19,11 +16,8 @@ import player
 
 class FooQt(QtWidgets.QMainWindow, design.Ui_MainWindow):
     def __init__(self, app):
-        # Это здесь нужно для доступа к переменным, методам
-        # и т.д. в файле design.py
         super().__init__()
         self.setupUi(self)  # Это нужно для инициализации нашего дизайна
-        # self.player = player
 
         # design updates
         self.themeCombo.addItems(QtWidgets.QStyleFactory.keys())
@@ -34,24 +28,28 @@ class FooQt(QtWidgets.QMainWindow, design.Ui_MainWindow):
         self.browseDirBtn.setIcon(self.style().standardIcon(QtWidgets.QStyle.SP_DialogOpenButton))
         self.rescanLibBtn.setIcon(self.style().standardIcon(QtWidgets.QStyle.SP_BrowserReload))
 
+
+        self.timer = QtCore.QTimer(self)
+        self.timer.setInterval(100)
+
         # config loading
         self.config = config.Config(self, app)
         library.init(self.config.getLibraryDir())
 
         # db and models init
-        db = QSqlDatabase.addDatabase('QSQLITE')
+        db = QtSql.QSqlDatabase.addDatabase('QSQLITE')
         db.setDatabaseName('db.sqlite3')
         db.open()
 
-        self.gridModel = QSqlTableModel(None, db)
-        self.gridModel.setTable('track')
-        self.gridModel.select()
-        self.tableView.setModel(self.gridModel)
+        self.tableModel = QtSql.QSqlTableModel(None, db)
+        self.tableModel.setTable('track')
+        self.tableModel.select()
+        self.tableView.setModel(self.tableModel)
         self.tableView.setSelectionBehavior(QtWidgets.QAbstractItemView.SelectRows)
 
         # self.treeModel = QStandardItemModel()
         self.treeModel = QtWidgets.QFileSystemModel()
-        self.treeModel.setFilter(QDir.AllDirs | QDir.NoDotAndDotDot)
+        self.treeModel.setFilter(QtCore.QDir.AllDirs | QtCore.QDir.NoDotAndDotDot)
         self.treeView.setModel(self.treeModel)
         # self.treeView.setRootIndex(self.treeModel.setRootPath(library.root_dir))
         # self.treeModel.setHeaderData()
@@ -66,27 +64,32 @@ class FooQt(QtWidgets.QMainWindow, design.Ui_MainWindow):
         self.searchEdit.textChanged.connect(self.searchChanged)
         self.treeView.clicked.connect(self.clickTreeView)
         self.tableView.doubleClicked.connect(self.play)
+        self.posSlider.setMaximum(1000)
+        self.posSlider.sliderMoved.connect(self.setPos)
+        self.posSlider.sliderPressed.connect(self.setPos)
+        self.timer.timeout.connect(self.updatePos)
         # self.tableView.currentChanged.connect(self.tableViewChanged)
 
         self.config.load()
 
-    def play(self, selInx: QModelIndex):
+    def getTrack(self, index: QtCore.QModelIndex):
         track = library.Track()
-        track.setAttrs([self.gridModel.data(self.gridModel.index(selInx.row(), col)) for col in range(0, self.gridModel.columnCount())])
+        track.setAttrs([self.tableModel.data(self.tableModel.index(index.row(), col)) for col in
+                        range(0, self.tableModel.columnCount())])
         print(track.__dict__)
-        player.play(track)
+        return track
 
-
+    def play(self, index: QtCore.QModelIndex):
+        player.play(self.getTrack(index))
         # index = self.gridModel.index(selInx.row(), library.Track().indexOf('full_path'))
-        # print(self.gridModel.data(index))
 
 
     def tableViewChanged(self, current, previous):
         print(current)
 
 
-    def clickTreeView(self, index: QModelIndex):
-        print(index.model().data(index, Qt.DisplayRole))
+    def clickTreeView(self, index: QtCore.QModelIndex):
+        print(index.model().data(index, QtCore.Qt.DisplayRole))
         # self.themeCombo.setCurrentIndex()
         # print(self.themeCombo.findText('cde'))
 
@@ -97,16 +100,44 @@ class FooQt(QtWidgets.QMainWindow, design.Ui_MainWindow):
 
 
     def searchChanged(self):
-        self.gridModel.setFilter(f'title LIKE "%{self.searchEdit.text()}%"')
+        self.tableModel.setFilter(f'title LIKE "%{self.searchEdit.text()}%"')
 
     def playBtnClick(self):
-        print([self.tableView.columnWidth(i) for i in range(0, self.gridModel.columnCount())])
+        print([self.tableView.columnWidth(i) for i in range(0, self.tableModel.columnCount())])
             # print(self.tableView.columnWidth(i))
 
         # print(self.geometry())
         # self.setGeometry(QRect(*self.config['window']))
 
 
+    def setPos(self):
+        """Set the movie position according to the position slider."""
+        # The vlc MediaPlayer needs a float value between 0 and 1, Qt uses
+        # integer variables, so you need a factor; the higher the factor, the
+        # more precise are the results (1000 should suffice).
+
+        # Set the media position to where the slider was dragged
+        self.timer.stop()
+        pos = self.posSlider.value()
+        player.mediaplayer.set_position(pos / 1000.0)
+        self.timer.start()
+
+    def updatePos(self):
+        # Set the slider's position to its corresponding media position
+        # Note that the setValue function only takes values of type int,
+        # so we must first convert the corresponding media position.
+        media_pos = int(player.mediaplayer.get_position() * 1000)
+        self.posSlider.setValue(media_pos)
+
+        # No need to call this function if nothing is played
+        if not player.mediaplayer.is_playing():
+            self.timer.stop()
+
+            # After the video finished, the play button stills shows "Pause",
+            # which is not the desired behavior of a media player.
+            # This fixes that "bug".
+            if not player.paused:
+                player.stop()
 
     def closeEvent(self, event):
         self.config.save()
